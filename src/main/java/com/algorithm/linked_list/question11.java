91049af72a9edea95fcbe897693842c852aa208c
琚安生 <juansheng@qijiayoudao.com>
2018-02-12 15:10:44 +0800

    add

--- src/main/java/com/algorithm/linked_list/question11.java
+++ src/main/java/com/algorithm/linked_list/question11.java
@@ -7,6 +7,12 @@ import java.util.Stack;
  * 如果最后不足k则直接返回
  */
 public class question11 {
+    /**
+     * 方法1
+     * @param head
+     * @param k
+     * @return
+     */
     public static SignNode reverseNode(SignNode head, int k){
         if (k < 2){
             return head;
@@ -44,6 +50,52 @@ public class question11 {
         return cur;
     }
 
+    /**
+     * 方法2
+     * @param head
+     * @param k
+     * @return
+     */
+    public static SignNode reverseNode2(SignNode head, int k){
+        if (k < 2){
+            return head;
+        }
+        SignNode pre = null;
+        SignNode cur = head;
+        SignNode next = null;
+        SignNode start = null;
+        int count = 1;
+        while (cur != null){
+            next = cur.next;
+            if (count == k){
+                start = pre == null ? head : pre.next;
+                head = pre == null ? cur : head;
+                resign2(pre, start, cur, cur.next);
+                pre = start;
+                count = 0;
+            }
+            count++;
+            cur = next;
+        }
+        return head;
+    }
+
+    public static void resign2(SignNode left, SignNode start, SignNode end, SignNode right){
+        SignNode pre = start;
+        SignNode next = null;
+        SignNode cur = start.next;
+        while (cur != right){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        if (left != null){
+            left.next = end;
+        }
+        start.next = right;
+    }
+
     public static void main(String[] args) {
         SignNode head = new SignNode(1);
         int i = 2;
@@ -54,7 +106,7 @@ public class question11 {
             i++;
         }
         cur.next = new SignNode(10);
-        SignNode newhead = reverseNode(head, 3);
+        SignNode newhead = reverseNode2(head, 3);
         System.out.println(newhead);
     }
 }
