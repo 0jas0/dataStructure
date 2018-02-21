43663d68673ca39298e4f78dc70fd9b08233ba6c
琚安生 <juansheng@qijiayoudao.com>
2018-02-21 11:42:54 +0800

    add

---
+++ src/main/java/com/algorithm/binary_tree/question2.java
@@ -0,0 +1,41 @@
+package com.algorithm.binary_tree;
+
+import java.util.Stack;
+
+/**
+ * 中序遍历一棵树，采用递归和非递归两种方式
+ */
+public class question2 {
+    /**
+     * 通过递归的方式遍历一棵二叉树
+     * @param root 根节点
+     */
+    public void inOrderRecur(BinaryTree root){
+        if (root == null){
+            return;
+        }
+        inOrderRecur(root.left);
+        System.out.println(root.value);
+        inOrderRecur(root.right);
+    }
+
+    /**
+     * 采用非递归的方式遍历一棵二叉树
+     * @param root 根节点
+     */
+    public void inOrderUnRecur(BinaryTree root){
+        if (root != null){
+            Stack<BinaryTree> stack = new Stack<BinaryTree>();
+            while (!stack.isEmpty() || root != null){
+                if (root != null){
+                    stack.push(root);
+                    root = root.left;
+                }else {
+                    BinaryTree binaryTree = stack.pop();
+                    System.out.println(binaryTree.value);
+                    root = root.right;
+                }
+            }
+        }
+    }
+}
