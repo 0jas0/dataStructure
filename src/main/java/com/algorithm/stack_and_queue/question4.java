eb2b2d58ed80dec489a477be30df06fceab8cd56
琚安生 <juansheng@qijiayoudao.com>
2018-06-30 22:24:58 +0800

    add

--- src/main/java/com/algorithm/stack_and_queue/question4.java
+++ src/main/java/com/algorithm/stack_and_queue/question4.java
@@ -7,6 +7,7 @@ import java.util.Stack;
  * 一个栈中元素类型为整型，想实现从栈顶到栈底的排序为由大到小，只能申请一个辅助栈实现
  */
 public class question4 {
+
     public static void order(Stack<Integer> stack){
         Stack<Integer> help = new Stack<Integer>();
         while (!stack.isEmpty()){
@@ -24,6 +25,7 @@ public class question4 {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<Integer>();
         stack.push(2);
+        stack.push(3);
         stack.push(3);
         stack.push(1);
         stack.push(4);
