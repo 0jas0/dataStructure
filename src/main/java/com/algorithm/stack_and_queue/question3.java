fb843cc271acd18db699e644c2f389842781cc47
琚安生 <juansheng@qijiayoudao.com>
2018-06-30 20:49:16 +0800

    add

--- src/main/java/com/algorithm/stack_and_queue/question3.java
+++ src/main/java/com/algorithm/stack_and_queue/question3.java
@@ -8,22 +8,24 @@ import java.util.Stack;
  * 将一个栈反转只使用递归操作
  */
 public class question3 {
+
     public static Integer getLastAndRemove(Stack<Integer> stack){
-        Integer num = stack.pop();
-        if (stack.isEmpty()){
-            return num;
+        if (stack.size() == 1){
+            return stack.pop();
         }
-        Integer last = getLastAndRemove(stack);
-        stack.push(num);
-        return last;
+        Integer element = stack.pop();
+        Integer lastElement = getLastAndRemove(stack);
+        stack.push(element);
+        return lastElement;
     }
+
     public static void revert(Stack<Integer> stack){
         if (stack.isEmpty()){
             return;
         }
-        Integer num = getLastAndRemove(stack);
+        Integer element = getLastAndRemove(stack);
         revert(stack);
-        stack.push(num);
+        stack.push(element);
     }
 
     public static void main(String[] args) {
@@ -32,8 +34,8 @@ public class question3 {
         stack.push(2);
         stack.push(3);
         stack.push(4);
+        System.out.println(stack);
         revert(stack);
         System.out.println(stack);
-        System.out.println(stack.peek());
     }
 }
